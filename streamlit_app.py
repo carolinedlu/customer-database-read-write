b2843e0b81daab2ce3168fe222dee01adedaa8d0
Caroline Frasca (Lu) <42614552+carolinedlu@users.noreply.github.com>
2022-05-02 18:31:34 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,11 +9,11 @@ ACCOUNT = st.secrets["ACCOUNT"]
 st.title("Customer Loyalty Program")
 st.header("Look up an existing customer")
 first_name = st.text_input("Customer's first name", placeholder="Frank")
-last_name = st.text_input("Customer's first name", placeholder="Slootman")
+last_name = st.text_input("Customer's last name", placeholder="Slootman")
 
 st.header("Add a new customer")
 
-con = snowflake.connector.connect(
+conn = snowflake.connector.connect(
     user=USER,
     password=PASSWORD,
     account=ACCOUNT,
@@ -21,3 +21,11 @@ con = snowflake.connector.connect(
 #         'QUERY_TAG': 'EndOfMonthFinancials',
     }
 )
+
+def search_db_for_cust(first_name, last_name):
+    cur = conn.cursor()
+    cur.execute('select * from customers')
+    to_print = cur.fetchmany(3)
+    st.write(to_print)
+
+search_for_cust = st.button("Search for customer", on_click=search_, args=(first_name, last_name)))
