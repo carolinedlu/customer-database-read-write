6f6eeccb155016f03f4df2358a4fe4ad9f9a9062
Caroline Frasca (Lu) <42614552+carolinedlu@users.noreply.github.com>
2022-05-02 18:01:31 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,20 @@
 import streamlit as st
+import snowflake.connector
+
+PASSWORD = st.secrets["snowflake_password"]
+WAREHOUSE = st.secrets["warehouse"]
+USER = st.secrets["user"]
+ACCOUNT = st.secrets("account")
 
 st.title("Customer Loyalty Program")
-
 st.header("Look up an existing customer")
-
 st.header("Add a new customer")
+
+con = snowflake.connector.connect(
+    user=USER,
+    password=PASSWORD,
+    account=ACCOUNT,
+    session_parameters={
+#         'QUERY_TAG': 'EndOfMonthFinancials',
+    }
+)
