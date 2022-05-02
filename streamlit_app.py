32f25f78497d111f471bc6501f489bf70fbce612
Caroline Frasca (Lu) <42614552+carolinedlu@users.noreply.github.com>
2022-05-02 19:59:44 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,13 +44,12 @@ if add_new_cust:
     
     cur = conn.cursor()
     cur.execute("INSERT INTO CUSTOMER_LOYALTY_PROGRAM.PUBLIC.CUSTOMERS VALUES (%s, %s)", (first_name, last_name))
-#     cur.execute(f"INSERT INTO CUSTOMER_LOYALTY_PROGRAM.PUBLIC.CUSTOMERS" \
-#                 f"VALUES ({first_name}, {last_name})")
-    st.write(cur.fetchall())
+    try:
+      query_id = cur.sfqid
+      while conn.is_still_running(conn.get_query_status_throw_if_error(query_id)):
+        time.sleep(1)
+    except ProgrammingError as err:
+      st.write('Programming Error: {0}'.format(err))
     
     if cur.rowcount==0:
         st.error('No such customer exists in the database')
-
-#     to_print = cur.fetchmany(3)
-#     st.write(to_print)
-    
