6d0eaa923fdf1fde2059482138c02401b1a0dba3
Caroline Frasca (Lu) <42614552+carolinedlu@users.noreply.github.com>
2022-05-02 20:52:21 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,9 +39,8 @@ if search_for_cust:
             else:
                 search_results += f"{first} {last}, "
 
-        cust_exists_message = "✅ One or more customer(s) exist in the database with the same first and last names.\nHere they are: "
+        cust_exists_message = "✅ One or more customer(s) exist in the database with the same first and last names.\n Here they are: "
         full_results = cust_exists_message + search_results
-        st.write(full_results)
         st.success(full_results)
 
 if add_new_cust:
